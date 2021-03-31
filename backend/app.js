db2bf9a17db283333783304823a57bc643812fd1
Guillaume Marteau <guillaumemarteau@hotmail.fr>
2021-03-31 14:12:07 +0200

    v0.1.0: routes signup et login implémentées

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const userRoutes = require("./routes/user");
 
 const app = express();
 
-mongoose.connect('mongodb+srv://gmarteau:Libertines5@sopekocko.byrk1.mongodb.net/SoPekocko?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://gmarteau:Libertines5@sopekocko.byrk1.mongodb.net/soPekocko?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(express.json());
+
+app.use("/api/auth", userRoutes);
 
 module.exports = app;
