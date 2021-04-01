a7314ec43d5484db12292e8a61912a676c12e85f
Guillaume Marteau <guillaumemarteau@hotmail.fr>
2021-04-01 15:45:11 +0200

    v0.3.0: contrôle d'accès implémenté (middleware auth)

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -3,11 +3,12 @@ const router = express.Router();
 
 const sauceCtrl = require("../controllers/sauce");
 const multer = require("../middleware/multer-config");
+const auth = require("../middleware/auth");
 
-router.post("/", multer, sauceCtrl.addSauce);
-router.put("/:id", sauceCtrl.modifySauce);
-router.get("/", sauceCtrl.getAllSauces);
-router.get("/:id", sauceCtrl.getOneSauce);
-router.delete("/:id", sauceCtrl.deleteSauce);
+router.post("/", auth, multer, sauceCtrl.addSauce);
+router.put("/:id", auth, sauceCtrl.modifySauce);
+router.get("/", auth, sauceCtrl.getAllSauces);
+router.get("/:id", auth, sauceCtrl.getOneSauce);
+router.delete("/:id", auth, sauceCtrl.deleteSauce);
 
 module.exports = router;
