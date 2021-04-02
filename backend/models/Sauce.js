9d1a3d4238e6477cecd44484de5b3c356bfd6e90
Guillaume Marteau <guillaumemarteau@hotmail.fr>
2021-04-02 14:55:37 +0200

    v0.4.1: ajout validation Regex bloquant caractères spécifiques au code dans le schema Sauce

--- backend/models/Sauce.js
+++ backend/models/Sauce.js
@@ -1,15 +1,21 @@
 const mongoose = require("mongoose");
 
+const noCodeSpecificCharacters = (input) => {
+    return /^[^=<>]*$/.test(input);
+};
+
+const invalidInput = new Error("Le champ contient des caractères non autorisés.");
+
 const sauceSchema = mongoose.Schema({
-    userId: {type: String},
-    name: {type: String},
-    manufacturer: {type: String},
-    description: {type: String},
-    mainPepper: {type: String},
-    imageUrl: {type: String},
-    heat: {type: Number},
-    likes: {type: Number},
-    dislikes: {type: Number},
+    userId: {type: String, required: true},
+    name: {type: String, required: true, validate: [noCodeSpecificCharacters, invalidInput]},
+    manufacturer: {type: String, required: true, validate: [noCodeSpecificCharacters, invalidInput]},
+    description: {type: String, required: true, validate: [noCodeSpecificCharacters, invalidInput]},
+    mainPepper: {type: String, required: true, validate: [noCodeSpecificCharacters, invalidInput]},
+    imageUrl: {type: String, required: true},
+    heat: {type: Number, required: true, min: 1, max: 10},
+    likes: {type: Number, required: true},
+    dislikes: {type: Number, required: true},
     usersLiked: {type: [String]},
     usersDisliked: {type: [String]}
 });
