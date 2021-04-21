f64d39f112b59b2f5245437790d757df2ed0f66e
Guillaume Marteau <guillaumemarteau@hotmail.fr>
2021-04-21 15:12:20 +0200

    v2.0.2: ajout x-xss-protection, ajout rate-limit à /login, test vuln avec snyk ok

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const path = require("path");
 require("dotenv").config();
 const helmet = require("helmet");
+const xXssProtection = require("x-xss-protection");
+const rateLimit = require("express-rate-limit");
 
 const userRoutes = require("./routes/user");
 const sauceRoutes = require("./routes/sauce");
@@ -17,7 +19,7 @@ mongoose.connect(process.env.DATABASE_URL,
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
@@ -25,9 +27,16 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 app.use(helmet());
+app.use(xXssProtection());
 
 app.use("/images", express.static(path.join(__dirname, "images")));
 
+const loginLimiter = rateLimit({
+  windowMs: 5*60*1000,
+  max: 5,
+});
+
+app.use("/api/auth/login", loginLimiter);
 app.use("/api/auth", userRoutes);
 app.use("/api/sauces", sauceRoutes);
 
